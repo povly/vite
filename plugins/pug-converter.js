b4846ea20463122b594f8faaa49f8f0214ef36d7
Anatoly <povly19995@gmail.com>
2025-05-10 07:07:17 +0300

    [refactor] Separation of pug conversion logic, correction of monitoring without a library

---
+++ plugins/pug-converter.js
@@ -0,0 +1,62 @@
+import fs from 'fs';
+import path from 'path';
+import { normalizePath } from 'vite';
+import pug from 'pug';
+
+const NODE_ENV = process.env.NODE_ENV;
+export default function PugConverter() {
+  const dirmame = normalizePath(path.join(__dirname, '..'));
+  const pugDir = normalizePath(path.join(dirmame, 'src/pug'));
+  const pugDirPages = normalizePath(path.join(pugDir, 'pages'));
+  const htmlDir = NODE_ENV !== 'dev' ? normalizePath(path.join(dirmame, 'src/html')) : normalizePath(path.join(dirmame, 'dist'));
+  function ensureDirectoryExists(dirPath) {
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath, { recursive: true });
+    }
+  }
+  function renderPugToHtml(pugPath) {
+    try {
+      return pug.renderFile(pugPath, {
+        pretty: true,
+        basedir: pugDir,
+      });
+    } catch (error) {
+      console.error(`⚠️ PUG rendering error:`, error.message);
+      return null;
+    }
+  }
+  function generateInitialHtml() {
+    ensureDirectoryExists(htmlDir);
+    fs.readdirSync(pugDirPages, {withFileTypes: true}).forEach(file => {
+      const _path = file.parentPath; // path file
+      const _name = file.name; // name file
+      // Check if pug at dir src/pug/pages
+      if (_path.includes(pugDirPages) && _name.endsWith('.pug')){
+        const name = _name.replace('.pug', '.html');
+        const html = renderPugToHtml(normalizePath(path.join(_path, _name)));
+        if (html){
+          fs.writeFileSync(
+            path.join(htmlDir, name),
+            html
+          )
+        }
+      }
+    });
+  }
+  return {
+    name: 'pug-converter',
+    configureServer(server) {
+      generateInitialHtml();
+      const watcher = fs.watch(
+        pugDir,
+        { recursive: true },
+        (eventType, filename) => {
+          if (filename && filename.endsWith('.pug')) {
+            generateInitialHtml();
+          }
+        }
+      )
+      server.httpServer?.once('close', () => watcher.close()); // Сервер закрывается, отключаем наблюдатель
+    }
+  }
+}
--- vite.config.js
+++ vite.config.js
@@ -1,89 +1,21 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { defineConfig } from "vite";
 import path from 'path';
-import fs from 'fs';
-import { fileURLToPath } from 'url';
-import FullReload from 'vite-plugin-full-reload';
-import pug from 'pug';
+import { defineConfig } from "vite";
+import PugConverter from './plugins/pug-converter';
 
 // Constants
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const PUG_DIR = path.join(__dirname, 'src/pug/pages');
-const HTML_DIR = path.join(__dirname, 'src/html');
+const NODE_ENV = process.env.NODE_ENV; // .env arguments
 
-// Utility functions
-function ensureDirectoryExistence(dirPath) {
-  if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath, { recursive: true });
-  }
-}
-
-function renderPugToHtml(pugPath) {
-  try {
-    return pug.renderFile(pugPath, {
-      pretty: true,
-      basedir: path.join(__dirname, 'src/pug'),
-    });
-  } catch (error) {
-    console.error(`⚠️ PUG rendering error:`, error.message);
-    return null;
-  }
-}
-
-function generateInitialHtml() {
-  ensureDirectoryExistence(HTML_DIR);
-
-  fs.readdirSync(PUG_DIR).forEach(file => {
-    if (file.endsWith('.pug')) {
-      const name = file.replace('.pug', '');
-      const html = renderPugToHtml(path.join(PUG_DIR, file));
-
-      if (html) {
-        fs.writeFileSync(
-          path.join(HTML_DIR, `${name}.html`),
-          html
-        );
-      }
-    }
-  });
-}
-// Initial HTML generation
-generateInitialHtml();
-
-// Vite configuration
 export default defineConfig({
-  root: 'src/html',
+  root: NODE_ENV !== 'dev' ? 'src/html' : 'dist',
   base: './',
   server: {
     host: true,
   },
   plugins: [
-    {
-      name: 'file-process',
-      handleHotUpdate({ file, server }) {
-        if (file.endsWith('.pug')) {
-          try {
-            const relativePath = path.relative(PUG_DIR, file);
-            const outputName = relativePath.replace('.pug', '.html');
-            const html = renderPugToHtml(file);
-            if (html) {
-              fs.writeFileSync(
-                path.join(HTML_DIR, outputName),
-                html
-              );
-              console.log(`♻️ Updated: ${outputName}`);
-            }
-            server.ws.send({ type: "full-reload" });
-          } catch (error) {
-            console.error('⚠️ Hot reload error:', error.message);
-          }
-        }
-        return [];
-      }
-    },
-    FullReload(['src/pug/**/*.pug'], { delay: 200 })
+    PugConverter(),
   ],
   resolve: {
     alias: {
@@ -91,3 +23,4 @@ export default defineConfig({
     },
   },
 });
+ 
